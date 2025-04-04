f89c6e5986b9d88d08d6258c0f25e008d2ad447a
johns1124 <johns1124@users.noreply.github.com>
2025-04-04 14:43:06 -0700

    Add files via upload

---
+++ 2-15.py
@@ -0,0 +1,11 @@
+#get item original price
+original_price = float(input("Items original price: "))
+
+#Calc the 20% discount
+discount = original_price  *0.2
+
+#Find sale prive
+sale_price = original_price - discount
+
+#print results
+print("The final price is: ",sale_price)
